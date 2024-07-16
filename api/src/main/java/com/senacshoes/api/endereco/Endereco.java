f9a6ab56e199703f6d0bfb1e468c74e3f85a8f77
Matthieu <matthieustmartinez@gmail.com>
2024-07-15 21:29:47 -0300

    Primeira persistencia dos dados

---
+++ api/src/main/java/com/senacshoes/api/endereco/Endereco.java
@@ -0,0 +1,11 @@
+package com.senacshoes.api.endereco;
+
+public class Endereco {
+    private String lougradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+}
---
+++ api/src/main/java/com/senacshoes/api/paciente/DadosCadastroPaciente.java
@@ -0,0 +1,8 @@
+package com.senacshoes.api.paciente;
+
+import com.senacshoes.api.endereco.DadosEndereco;
+
+public record DadosCadastroPaciente(String nome, String email, String telefone, String cpf,
+        DadosEndereco endereco) {
+
+}
