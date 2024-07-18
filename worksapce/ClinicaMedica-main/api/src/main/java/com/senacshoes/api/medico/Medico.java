f149079b34e59d639e74b72d83144bdb39ca8a0a
Matthieu <166752810+Manomafo@users.noreply.github.com>
2024-07-17 21:35:14 -0300

    Atualização do dia 17/07

---
+++ worksapce/ClinicaMedica-main/api/src/main/java/com/senacshoes/api/medico/Medico.java
@@ -0,0 +1,13 @@
+package com.senacshoes.api.medico;
+
+import javax.print.DocFlavor.STRING;
+
+public class Medico {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String crm;
+    private Especialidades especialidades;
+    private String endereco;
+}
